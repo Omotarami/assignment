e0b87b4cb7a2286df55f9ace76c8c7412ec97f0f
Omotara <awofesotitilola@gmail.com>
2024-08-02 19:10:09 +0100

    java assignment

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,28 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Principal: ");
+		int Principal = sc.nextInt();
+		System.out.print("Enter R: ");
+		Float R = sc.nextFloat();
+		System.out.print("Enter n: ");
+		int n = sc.nextInt();
+		double M = Principal *(R *Math.pow(1+R, n))/
+				(Math.pow(1+R, n)-1);
+//		System.out.println("M = " + M);
+		
+		NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.US);
+		String dollar = currency.format(M);
+		System.out.print(dollar);
+		
+	
+		
+		
+		
+	}
     
 }
